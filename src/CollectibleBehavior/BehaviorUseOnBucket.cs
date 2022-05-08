3f906e1fe165282a15d39823363c6433bf26d0a0
Craluminum2413 <beware.14b@gmail.com>
2022-05-09 00:39:15 +0300

    remove unnecessary usings

--- src/CollectibleBehavior/BehaviorUseOnBucket.cs
+++ src/CollectibleBehavior/BehaviorUseOnBucket.cs
@@ -1,13 +1,8 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Vintagestory.API.Client;
 using Vintagestory.API.Common;
 using Vintagestory.API.Datastructures;
-using Vintagestory.API.MathTools;
-using Vintagestory.API.Server;
 using Vintagestory.API.Util;
 using Vintagestory.GameContent;
 
