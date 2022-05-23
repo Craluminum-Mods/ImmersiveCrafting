93cff1b04c69a1180eadc1d52402f643241a00a5
Craluminum2413 <beware.14b@gmail.com>
2022-05-23 10:37:01 +0300

    Remove unnecessary things

--- src/CollectibleBehavior/BehaviorSealCrock.cs
+++ src/CollectibleBehavior/BehaviorSealCrock.cs
@@ -63,32 +63,25 @@ namespace ImmersiveCrafting
       var world = byEntity.World;
 
       var blockPos = blockSel.Position;
-      var block = byEntity.World.BlockAccessor.GetBlock(blockPos);
+      var block = world.BlockAccessor.GetBlock(blockPos);
       var blockEntity = world.BlockAccessor.GetBlockEntity(blockPos);
 
-      IPlayer byPlayer = null;
-      if (byEntity is EntityPlayer) byPlayer = world.PlayerByUid(((EntityPlayer)byEntity).PlayerUID);
-      if (byPlayer == null) return;
-
-      if (firstEvent && handHandling != EnumHandHandling.PreventDefault)
+      if (block is BlockGroundStorage)
       {
-        if (block is BlockGroundStorage)
-        {
-          var begs = blockEntity as BlockEntityGroundStorage;
-          ItemSlot gsslot = begs.GetSlotAt(blockSel);
-          if (gsslot == null || gsslot.Empty) return;
-          ItemStack crock = gsslot.Itemstack;
+        var begs = blockEntity as BlockEntityGroundStorage;
+        ItemSlot gsslot = begs.GetSlotAt(blockSel);
+        if (gsslot == null || gsslot.Empty) return;
+        ItemStack crock = gsslot.Itemstack;
 
-          if (crock?.Collectible is BlockCrock && crock.Attributes.GetBool("sealed") == false)
-          {
-            crock.Attributes.SetBool("sealed", true);
-            slot.TakeOut(1);
-            slot.MarkDirty();
-            gsslot.MarkDirty();
-            begs.updateMeshes();
-            begs.MarkDirty(true);
-            handHandling = EnumHandHandling.PreventDefault;
-          }
+        if (crock?.Collectible is BlockCrock && crock.Attributes.GetBool("sealed") == false)
+        {
+          crock.Attributes.SetBool("sealed", true);
+          slot.TakeOut(1);
+          slot.MarkDirty();
+          gsslot.MarkDirty();
+          begs.updateMeshes();
+          begs.MarkDirty(true);
+          handHandling = EnumHandHandling.PreventDefault;
         }
       }
     }
