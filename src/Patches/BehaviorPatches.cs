22f7839876a82be0523671aaa1cc5a05db8f0f35
Craluminum2413 <beware.14b@gmail.com>
2023-04-03 14:31:03 +0300

    Moved patches to AssetsFinalize

--- src/Patches/BehaviorPatches.cs
+++ src/Patches/BehaviorPatches.cs
@@ -2,23 +2,18 @@ using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using Vintagestory.API.Common;
 using Vintagestory.API.Datastructures;
-using Vintagestory.API.Server;
 using Vintagestory.API.Util;
 
 namespace ImmersiveCrafting
 {
   public class BehaviorPatches : ModSystem
   {
-    ICoreAPI api;
-
-    public override void Start(ICoreAPI api)
+    public override void AssetsFinalize(ICoreAPI api)
     {
-      this.api = api;
-
-      if (api is ICoreServerAPI sapi) sapi.Event.AssetsFinalizers += AppendBehaviors;
+      AppendBehaviors(api);
     }
 
-    private void AppendBehaviors()
+    private void AppendBehaviors(ICoreAPI api)
     {
       var beeswaxItem = api.World.GetItem(new AssetLocation("beeswax"));
       beeswaxItem.CollectibleBehaviors = beeswaxItem.CollectibleBehaviors.Append(new CollectibleBehaviorSealCrock(beeswaxItem));
@@ -28,19 +23,19 @@ namespace ImmersiveCrafting
       fatItem.CollectibleBehaviors = fatItem.CollectibleBehaviors.Append(new CollectibleBehaviorSealCrock(fatItem));
 
       var bandageCleanItem = api.World.GetItem(new AssetLocation("bandage-clean"));
-      AppendCollectibleBehaviorInSimpleWay(bandageCleanItem, "immersivecrafting:config/useonliquidcontainer/bandage.json", new CollectibleBehaviorUseOnLiquidContainer(bandageCleanItem));
+      AppendCollectibleBehaviorInSimpleWay(api, bandageCleanItem, "immersivecrafting:config/useonliquidcontainer/bandage.json", new CollectibleBehaviorUseOnLiquidContainer(bandageCleanItem));
 
       var pineappleBlock = api.World.GetBlock(new AssetLocation("pineapple"));
-      AppendBlockBehaviorInSimpleWay(pineappleBlock, "immersivecrafting:config/removebytool/pineapple.json", new BlockBehaviorRemoveByTool(pineappleBlock));
+      AppendBlockBehaviorInSimpleWay(api, pineappleBlock, "immersivecrafting:config/removebytool/pineapple.json", new BlockBehaviorRemoveByTool(pineappleBlock));
 
       var pumpkinBlock = api.World.GetBlock(new AssetLocation("pumpkin-fruit-4"));
-      AppendBlockBehaviorInSimpleWay(pumpkinBlock, "immersivecrafting:config/removebytool/pumpkin.json", new BlockBehaviorRemoveByTool(pumpkinBlock));
+      AppendBlockBehaviorInSimpleWay(api, pumpkinBlock, "immersivecrafting:config/removebytool/pumpkin.json", new BlockBehaviorRemoveByTool(pumpkinBlock));
 
       var chalkStoneBlock = api.World.GetBlock(new AssetLocation("loosestones-chalk-free"));
       var limeStoneBlock = api.World.GetBlock(new AssetLocation("loosestones-limestone-free"));
       const string loosestonesProperties = "immersivecrafting:config/removebytool/loosestones.json";
-      AppendBlockBehaviorInSimpleWay(chalkStoneBlock, loosestonesProperties, new BlockBehaviorRemoveByTool(chalkStoneBlock));
-      AppendBlockBehaviorInSimpleWay(limeStoneBlock, loosestonesProperties, new BlockBehaviorRemoveByTool(limeStoneBlock));
+      AppendBlockBehaviorInSimpleWay(api, chalkStoneBlock, loosestonesProperties, new BlockBehaviorRemoveByTool(chalkStoneBlock));
+      AppendBlockBehaviorInSimpleWay(api, limeStoneBlock, loosestonesProperties, new BlockBehaviorRemoveByTool(limeStoneBlock));
 
       if (api.ModLoader.IsModEnabled("ancienttools"))
       {
@@ -51,13 +46,13 @@ namespace ImmersiveCrafting
       if (api.ModLoader.IsModEnabled("expandedfoods"))
       {
         var item = api.World.GetItem(new AssetLocation("expandedfoods", "soyprep-shelled"));
-        AppendCollectibleBehaviorInSimpleWay(item, "immersivecrafting:config/useonliquidcontainer/expandedfoods_soyprep.json", new CollectibleBehaviorUseOnLiquidContainer(item));
+        AppendCollectibleBehaviorInSimpleWay(api, item, "immersivecrafting:config/useonliquidcontainer/expandedfoods_soyprep.json", new CollectibleBehaviorUseOnLiquidContainer(item));
       }
 
       if (api.ModLoader.IsModEnabled("potatoes"))
       {
         var item = api.World.GetItem(new AssetLocation("potatoes", "cornflour"));
-        AppendCollectibleBehaviorInSimpleWay(item, "immersivecrafting:config/useonliquidcontainer/morecrops_cornflour.json", new CollectibleBehaviorUseOnLiquidContainer(item));
+        AppendCollectibleBehaviorInSimpleWay(api, item, "immersivecrafting:config/useonliquidcontainer/morecrops_cornflour.json", new CollectibleBehaviorUseOnLiquidContainer(item));
       }
 
       foreach (CollectibleObject collobj in api.World.Collectibles)
@@ -66,7 +61,7 @@ namespace ImmersiveCrafting
 
         if (collobj.Code.BeginsWith("game", "sand") && collobj.Code.EndVariant() == collobj.Variant["rock"])
         {
-          AppendCollectibleBehaviorInSimpleWay(collobj, "immersivecrafting:config/useonliquidcontainer/sand.json", new CollectibleBehaviorUseOnLiquidContainer(collobj));
+          AppendCollectibleBehaviorInSimpleWay(api, collobj, "immersivecrafting:config/useonliquidcontainer/sand.json", new CollectibleBehaviorUseOnLiquidContainer(collobj));
         }
 
         if (collobj.Code.BeginsWith("game", "flour"))
@@ -85,13 +80,13 @@ namespace ImmersiveCrafting
       }
     }
 
-    private void AppendCollectibleBehaviorInSimpleWay(CollectibleObject collobj, string propertiesFromJson, CollectibleBehavior instance)
+    private void AppendCollectibleBehaviorInSimpleWay(ICoreAPI api, CollectibleObject collobj, string propertiesFromJson, CollectibleBehavior instance)
     {
       instance.Initialize(new JsonObject(api.Assets.Get<JToken>(new AssetLocation(propertiesFromJson))));
       collobj.CollectibleBehaviors = collobj.CollectibleBehaviors.Append(instance);
     }
 
-    private void AppendBlockBehaviorInSimpleWay(Block block, string propertiesFromJson, BlockBehavior instance)
+    private void AppendBlockBehaviorInSimpleWay(ICoreAPI api, Block block, string propertiesFromJson, BlockBehavior instance)
     {
       instance.Initialize(new JsonObject(api.Assets.Get<JToken>(new AssetLocation(propertiesFromJson))));
       block.CollectibleBehaviors = block.CollectibleBehaviors.Append(instance);
